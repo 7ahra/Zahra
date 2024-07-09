995247f77df86ec456cf70647d332fafabbbc1e2
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-09 11:15:04 -0500

    evaluation separated

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from utils.evaluations import evaluate, evaluate_errors
 from utils.log_texts import CYAN, LOG, RESET, SUCCESS
-from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 
 def train(model, train_loader, criterion, optimizer, device):
     model.train()
@@ -20,47 +20,6 @@ def train(model, train_loader, criterion, optimizer, device):
     
     epoch_loss = running_loss / len(train_loader.dataset)
     return epoch_loss
-
-def evaluate(model, test_loader, device):
-    model.eval()
-    all_preds = []
-    all_labels = []
-    with torch.no_grad():
-        for inputs, labels in test_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-            all_preds.extend(preds.cpu().numpy())
-            all_labels.extend(labels.cpu().numpy())
-    
-    accuracy = accuracy_score(all_labels, all_preds)
-    precision = precision_score(all_labels, all_preds, average='weighted')
-    recall = recall_score(all_labels, all_preds, average='weighted')
-    f1 = f1_score(all_labels, all_preds, average='weighted')
-    return accuracy, precision, recall, f1
-
-def evaluate_errors(model, test_loader, device):
-    model.eval()
-    top1_correct = 0
-    top5_correct = 0
-    total_samples = 0
-
-    with torch.no_grad():
-        for inputs, labels in test_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            _, preds = torch.topk(outputs, k=5, dim=1)
-            total_samples += labels.size(0)
-        
-            top1_correct += (preds[:, 0] == labels).sum().item()
-
-            top5_correct += torch.tensor([label in pred for label, pred in zip(labels, preds)]).sum().item()
-
-    top1_accuracy = top1_correct / total_samples
-    top5_accuracy = top5_correct / total_samples
-
-    return 1 - top1_accuracy, 1 - top5_accuracy 
-
 
 def run_training(model_fn, train_loader, test_loader, num_classes, num_epochs=10):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
