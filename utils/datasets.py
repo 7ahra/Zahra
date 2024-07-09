93dd5786af909ef7e0a9e16671c604ec4786c5f9
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-09 13:03:05 -0500

    imagenet dataset updated

--- utils/datasets.py
+++ utils/datasets.py
@@ -32,8 +32,8 @@ def get_datasets(dataset_name):
         test_dataset  = datasets.MNIST(root='./datasets', train=False, download=True, transform=transform_mnist)
         num_classes = 10
     elif dataset_name == "ImageNet":
-        train_dataset = torch.utils.data.DataLoader(ImageFolder('./datasets/', transform=transform), batch_size=16, shuffle=True)
-        test_dataset  = torch.utils.data.DataLoader(ImageFolder('./datasets/', transform=transform), batch_size=16, shuffle=False)
+        train_dataset = torch.utils.data.DataLoader(ImageFolder('./datasets/imagenet-object-localization-challenge/ILSVRC/Data/CLS-LOC/train', transform=transform), batch_size=16, shuffle=True)
+        test_dataset  = torch.utils.data.DataLoader(ImageFolder('./datasets/imagenet-object-localization-challenge/ILSVRC/Data/CLS-LOC/test', transform=transform), batch_size=16, shuffle=False)
         num_classes = 1000
     else:
         raise ValueError("Unsupported dataset. Please choose from 'CIFAR10', 'CIFAR100', 'MNIST', or 'ImageNet'.")
