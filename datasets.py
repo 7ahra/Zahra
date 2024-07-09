90aebb4713623453a588f7bae6694d9680e4a6cd
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-09 10:16:39 -0500

    small datasets downloader and validators

---
+++ datasets.py
@@ -0,0 +1,33 @@
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+def get_datasets(dataset_name):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    transform_mnist = transforms.Compose([
+        transforms.Grayscale(3),
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    if dataset_name == 'CIFAR10':
+        train_dataset = datasets.CIFAR10(root='./datasets', train=True, download=True, transform=transform)
+        test_dataset = datasets.CIFAR10(root='./datasets', train=False, download=True, transform=transform)
+        num_classes = 10
+    elif dataset_name == 'CIFAR100':
+        train_dataset = datasets.CIFAR100(root='./datasets', train=True, download=True, transform=transform)
+        test_dataset = datasets.CIFAR100(root='./datasets', train=False, download=True, transform=transform)
+        num_classes = 100
+    elif dataset_name == 'MNIST':
+        train_dataset = datasets.MNIST(root='./datasets', train=True, download=True, transform=transform_mnist)
+        test_dataset = datasets.MNIST(root='./datasets', train=False, download=True, transform=transform_mnist)
+        num_classes = 10
+    else:
+        raise ValueError("Unsupported dataset. Please choose from 'CIFAR10', 'CIFAR100', or 'MNIST'.")
+
+    return train_dataset, test_dataset, num_classes
