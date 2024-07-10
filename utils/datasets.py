861c29708abcd2e7ed900afcfdf9daed79ba1efc
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-10 14:32:38 -0500

    all done

--- utils/datasets.py
+++ utils/datasets.py
@@ -5,27 +5,34 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from torchvision.datasets import ImageFolder
 
-def get_datasets(dataset_name):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
-    transform_mnist = transforms.Compose([
-        transforms.Grayscale(3),
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
+def collate_fn(batch):
+    # Check if images have different sizes and resize them
+    imgs = [img for img, _ in batch]
+    targets = [target for _, target in batch]
+
+    # Resize images to a consistent size
+    imgs_resized = []
+    for img in imgs:
+        img_resized = transform(img)
+        imgs_resized.append(img_resized)
+
+    return imgs_resized, targets
+
+def get_datasets(dataset_name):
 
     if dataset_name == 'CIFAR10':
         train_dataset = datasets.CIFAR10(root='./datasets', train=True, download=True, transform=transform)
         test_dataset = datasets.CIFAR10(root='./datasets', train=False, download=True, transform=transform)
         num_classes  = 10
     elif dataset_name == 'MSCOCO':
-        train_dataset = datasets.CocoDetection(root='./datasets', annFile='./datasets', transform=transform_mnist)
-        test_dataset  = datasets.CocoDetection(root='./datasets', annFile='./datasets',transform=transform_mnist)
+        train_dataset = datasets.CocoDetection(root='./datasets/ms_coco/train2017', annFile='./datasets/ms_coco/annotations/instances_train2017.json', transform=None)
+        test_dataset  = datasets.CocoDetection(root='./datasets/ms_coco/test2017',  annFile='./datasets/ms_coco/annotations/image_info_test2017.json',transform=None)
         num_classes = 10
     elif dataset_name == "ImageNet":
         train_dataset = ImageFolder('./datasets/imagenet-object-localization-challenge/ILSVRC/Data/CLS-LOC/train', transform=transform)
