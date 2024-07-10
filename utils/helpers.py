36c86e11d245936042586a9155cbb98576b1f678
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-10 11:17:52 -0500

    downloader and extractor

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,8 @@
-import time
+import requests
+import os
+from tqdm import tqdm
+import zipfile
+import tarfile
 
 RED = '\033[31m'
 GREEN = '\033[32m'
@@ -33,3 +37,41 @@ def format_duration(seconds):
         duration_str += f"{seconds} Second(s)"
     
     return duration_str.strip()
+
+def download_file(url, dest_folder):
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    filename = os.path.join(dest_folder, url.split('/')[-1])
+
+    with open(filename, 'wb') as file, tqdm(
+        desc=f'{LOG}Downloading {filename}',
+        total=total_size,
+        unit='B',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for chunk in response.iter_content(chunk_size=1024):
+            size = file.write(chunk)
+            bar.update(size)
+    
+    return filename
+
+def extract_file(filepath, dest_folder):
+    if filepath.endswith('.zip'):
+        with zipfile.ZipFile(filepath, 'r') as zip_ref:
+            zip_info_list = zip_ref.infolist()
+            total_files = len(zip_info_list)
+
+            with tqdm(total=total_files, desc=f'{LOG}Extracting {filepath}', unit='file') as bar:
+                for file_info in zip_info_list:
+                    zip_ref.extract(file_info, dest_folder)
+                    bar.update(1)
+    elif filepath.endswith(('.tar.gz', '.tgz', '.tar')):
+        with tarfile.open(filepath, 'r:*') as tar_ref:
+            tar_info_list = tar_ref.getmembers()
+            total_files = len(tar_info_list)
+
+            with tqdm(total=total_files, desc=f'{LOG}Extracting {filepath}', unit='file') as bar:
+                for file_info in tar_info_list:
+                    tar_ref.extract(file_info, dest_folder)
+                    bar.update(1)
